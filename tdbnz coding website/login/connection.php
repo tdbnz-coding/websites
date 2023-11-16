3dc62c2ccd6b558efc91e3f0096bf71cace24366
tdbnz-coding <bise@smartbusiness.me>
2023-11-17 12:34:57 +1300

    update

---
+++ tdbnz coding website/login/connection.php
@@ -0,0 +1,6 @@
+<?php
+
+$dbhost = "https://sql306.infinityfree.com/"
+$dbuser = "https://sql306.infinityfree.com/"
+$dbpass = "https://sql306.infinityfree.com/"
+$dbname = "https://sql306.infinityfree.com/"
