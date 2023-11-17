048a1eb07b71a0529a99a3bd7c20a64be79771a8
tdbnz-coding <bise@smartbusiness.me>
2023-11-17 14:16:13 +1300

    Update connection.php

--- tdbnz coding website/login/connection.php
+++ tdbnz coding website/login/connection.php
@@ -3,7 +3,7 @@
 $dbhost = "sql306.infinityfree.com";
 $dbuser = "epiz_33967205";
 $dbpass = "4b54EqoaH1F";
-$dbname = "login_sample_db";
+$dbname = "epiz_33967205_login_sample_db";
 
 
 if(!$con = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname))
